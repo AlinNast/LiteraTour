ffcd9ec99af2baa0689b137474b91e463df449cd
RazvanIonutIancu <riiancu94@gmail.com>
2026-03-30 16:23:18 +0200

    Added Camera

--- litera-tour-the-game/scripts/PlayerSelectionScreen.cs
+++ litera-tour-the-game/scripts/PlayerSelectionScreen.cs
@@ -98,5 +98,6 @@ public partial class PlayerSelectionScreen : Control
 		GD.Print("Selected number of players: " + numPlayers);
 		// Then change to the next scene, such as the level selection screen
 		//GetTree().ChangeSceneToFile("Scenes/LevelSelectionScreen.tscn");
+		GameInstance.Instance.StartGame(numPlayers);
 	}
 }
