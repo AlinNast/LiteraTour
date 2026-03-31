2824c2a3ba176567c88199eefc1a562f86105832
dunnowla <leosod-5@student.ltu.se>
2026-03-31 15:51:32 +0200

    Spread fixed

--- litera-tour-the-game/TestGun.cs
+++ litera-tour-the-game/TestGun.cs
@@ -1,19 +1,66 @@
 using Godot;
 using System;
+using System.ComponentModel;
+using System.Diagnostics;
 
 public partial class TestGun : Node3D
 {
+
+	public Random rng = new Random();
 	[Export] public float bulletSpeed;
 	[Export] public PackedScene bulletPrefab;
 	[Export] public Marker3D shootPos;
+	[Export] public int shotgunBullets;
+	[Export] public int gunType;// Semi = 0, Auto = 1, Shotgun = 2
+	[Export] public float fireRate;
+	[Export] public float spread;
+
+	private float shootTimer = 1;
+	private bool isShooting = false;
 	
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if(Input.IsActionJustPressed("shoot"))
+		if(gunType == 0)
 		{
-			Shoot(); // Pew Pew
+			if(Input.IsActionJustPressed("shoot") && shootTimer > fireRate)
+			{
+				isShooting = true;
+				Shoot();
+				shootTimer = 0;
+			}
+		}
+		else if(gunType == 1)
+		{
+			if(Input.IsActionPressed("shoot")&& shootTimer > fireRate)
+			{
+				isShooting = true;
+				Shoot();
+				shootTimer = 0;
+			}
+		}
+		else if (gunType == 2)
+		{
+			if(Input.IsActionJustPressed("shoot")&& shootTimer > fireRate)
+			{
+				isShooting = true;
+				int x = 0;
+				while (x < shotgunBullets)
+				{
+					Shoot();
+					x++;
+				}
+				shootTimer = 0;
+			}
+		}
+		if(isShooting)
+		{
+			shootTimer += (float) delta;
+			if(shootTimer > fireRate)
+			{
+				isShooting = false;
+			}
 		}
 	}
 
@@ -21,11 +68,12 @@ public partial class TestGun : Node3D
 	{
 		if (bulletPrefab == null) return;
 		Bullet newBullet = bulletPrefab.Instantiate<Bullet>(); // Create bullet
-
 		newBullet.GlobalTransform = shootPos.GlobalTransform; // Set position
+		Vector3 forward = -shootPos.GlobalBasis.Z;
+		Vector3 right = shootPos.GlobalBasis.X;
+		float spreadAmount = (float)GD.RandRange(-spread, spread) * gunType;
+		newBullet.direction = (forward+(right*spreadAmount)).Normalized();
 		newBullet.speed = bulletSpeed; // Set speed
-		newBullet.direction = -shootPos.GlobalBasis.Z; // Set shoot direction
-
 		GetTree().Root.AddChild(newBullet); // Add to world
 	}
 }
