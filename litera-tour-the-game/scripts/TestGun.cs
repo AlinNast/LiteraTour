ce6c1b30e8afd75811eeb4ac13b652db986cecc3
dunnowla <leosod-5@student.ltu.se>
2026-04-02 14:21:52 +0200

    Changes to gun script

--- litera-tour-the-game/scripts/TestGun.cs
+++ litera-tour-the-game/scripts/TestGun.cs
@@ -5,48 +5,51 @@ using System.Diagnostics;
 
 public partial class TestGun : Node3D
 {
-
 	public Random rng = new Random();
-	[Export] public float bulletSpeed;
+
+	[ExportGroup("Gun Setup")]
 	[Export] public PackedScene bulletPrefab;
 	[Export] public Marker3D shootPos;
-	[Export] public int shotgunBullets;
-	[Export] public int gunType;// Semi = 0, Auto = 1, Shotgun = 2
-	[Export] public float fireRate;
-	[Export] public float spread;
+	[ExportGroup("Upgradeable Data")]
+	[Export] public bool isSemi, isAuto; // Weapon types
+	[Export] public float bulletSpeed; // Speed of the bullet
+	[Export] public float fireRate; // Time between shots
+	[Export] public float spread; // The amount the bullets offset
+	[Export] public int bulletAmount = 1; // How many bullets the weapons shoot
 
 	private float shootTimer = 99999;
 	private bool isShooting = false;
-	
+
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if(gunType == 0)
+		if(Input.IsActionJustPressed("SwitchMode"))
+		{
+			isAuto = !isAuto;
+			isSemi = !isSemi;
+		}
+		if(isSemi)
 		{
 			if(Input.IsActionJustPressed("shoot") && shootTimer > fireRate)
 			{
 				isShooting = true;
-				Shoot();
+				int x = 0;
+				while (x < bulletAmount)
+				{
+					Shoot();
+					x++;
+				}
 				shootTimer = 0;
 			}
 		}
-		else if(gunType == 1)
+		else if(isAuto)
 		{
 			if(Input.IsActionPressed("shoot")&& shootTimer > fireRate)
 			{
 				isShooting = true;
-				Shoot();
-				shootTimer = 0;
-			}
-		}
-		else if (gunType == 2)
-		{
-			if(Input.IsActionJustPressed("shoot")&& shootTimer > fireRate)
-			{
-				isShooting = true;
 				int x = 0;
-				while (x < shotgunBullets)
+				while (x < bulletAmount)
 				{
 					Shoot();
 					x++;
@@ -71,7 +74,7 @@ public partial class TestGun : Node3D
 		newBullet.GlobalTransform = shootPos.GlobalTransform; // Set position
 		Vector3 forward = -shootPos.GlobalBasis.Z;
 		Vector3 right = shootPos.GlobalBasis.X;
-		float spreadAmount = (float)GD.RandRange(-spread, spread) * gunType;
+		float spreadAmount = (float)GD.RandRange(-spread, spread) * 2;
 		newBullet.direction = (forward+(right*spreadAmount)).Normalized();
 		newBullet.speed = bulletSpeed; // Set speed
 		GetTree().Root.AddChild(newBullet); // Add to world
